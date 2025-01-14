6a28257671d48554683ad82c7d9cc119c888d475
BadHappy <1196554262@qq.com>
2025-01-14 22:43:48 +0800

    Site updated: 2025-01-14 22:43:47

--- config/js/img.js
+++ config/js/img.js
@@ -1,7 +1,19 @@
 document.addEventListener('hexo-blog-encrypt:decrypted', function () {
+    // 获取所有图片元素
     const imgs = document.querySelectorAll('img');
+
     imgs.forEach(img => {
+        // 如果图片使用懒加载，获取其 data-src 属性
+        const lazySrc = img.getAttribute('data-src');
+        if (lazySrc) {
+            img.setAttribute('src', lazySrc);
+            img.removeAttribute('data-src'); // 移除懒加载属性
+        }
+
+        // 如果图片直接使用 src 属性，重新设置 src 来触发加载
         const src = img.getAttribute('src');
-        if (src) img.setAttribute('src', src); // 重新触发图片加载
+        if (src) {
+            img.setAttribute('src', src);
+        }
     });
 });
